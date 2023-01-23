0e28a4410d16955c203d5f2b689ebbdbe2077c8c
brano <branozeru@gmail.com>
2023-01-23 21:04:29 +0200

    Added person class -  - load table rows to a "Person" list  - updated an person object and update his row  - add a Person to the list and add him to the table

---
+++ src/main/java/org/example/MyConnection.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class MyConnection {
+
+    private String host = "localhost";
+    private String port = "5432";
+    private String user = "temp_login";
+    private String pass = "123";
+    private String database = "postgres";
+
+    public PostgreSQLConnection myCon(){
+        return new PostgreSQLConnection(host, port, user, pass, database);
+    }
+
+}
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,69 @@
+package org.example;
+
+import java.util.List;
+
+public class Person {
+
+    int ID;
+    String first_name;
+    String last_name;
+    Person(int m_id, String m_first_name, String m_last_name){
+
+        ID = m_id;
+        first_name = m_first_name;
+        last_name = m_last_name;
+
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void updatePerson(String property, String toSet){
+
+
+        PostgreSQLConnection psqlc = (new MyConnection()).myCon();
+
+        switch (property){
+            case "ID":
+                this.ID = Integer.parseInt(toSet);
+                break;
+            case "first_name":
+                this.first_name = toSet;
+                break;
+            case "last_name":
+                this.last_name = toSet;
+                break;
+        }
+
+        psqlc.update("USER", property, toSet, "ID", String.valueOf(this.ID));
+
+    }
+
+    public String outPut() {
+
+        return "\nID: " + getID() + "\nfirst_name: " + getFirst_name() + "\nlast_name: " + getLast_name();
+
+    }
+
+}
